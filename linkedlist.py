955a11690d677d7574209fde8decaa43f2d11f12
Carrie Li <carrie.t.li@gmail.com>
2017-05-29 07:01:16 +0000

    Completd Runtime, Queue, Stack

--- linkedlist.py
+++ linkedlist.py
@@ -76,9 +76,12 @@ class LinkedList(object):
             fish
         """
 
-        # FIXME
+        current = self.head
 
-        pass
+        while current is not None:
+            print current.data
+            current = current.next
+
 
     def get_node_by_index(self, idx):
         """Return a node with the given index::
@@ -100,9 +103,10 @@ class LinkedList(object):
             Exception: List not long enough
         """
 
-        # FIXME
 
-        pass
+        print self.data
+
+
 
 if __name__ == "__main__":
     import doctest
